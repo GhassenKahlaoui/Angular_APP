f4bf0ae7a8632510125af95cc3d5362cecb653a6
Mohamed Ghassen <ghassenkahlaoui@gmail.com>
2019-01-25 08:42:29 +0100

    ajouter des autre services comme put et get

--- src/app/components/mc/mc.component.ts
+++ src/app/components/mc/mc.component.ts
@@ -8,17 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mc.component.css']
 })
 export class McComponent implements OnInit {
-users:any[];
+posts:any[];
 
   constructor(private http : Http) {
 
    }
 
   ngOnInit() {
-this.http.get("https://jsonplaceholder.typicode.com/users").subscribe( response => {
-  this.users = response.json()
+this.http.get("https://jsonplaceholder.typicode.com/posts").subscribe( response => {
+  this.posts = response.json()
 })
   }
 
-
+  ajouter(input : HTMLInputElement) {
+    let post = {title: input.value, id : ''}
+      this.http.post("https://jsonplaceholder.typicode.com/posts",JSON.stringify(post)).
+      subscribe(response=>{
+          this.posts.splice(0,0,post)
+      })
+  }
+  update(post,inputTitle) {
+    let updatepost = {title : inputTitle ,  id :post.id};
+      this.http.put("https://jsonplaceholder.typicode.com/posts" + "/" + post.id , JSON.stringify(updatepost)).subscribe(response=> {
+        // update current view 
+        let index = this.posts.indexOf(post);
+        this.posts[index] = updatepost;
+    })
+  }
 }
